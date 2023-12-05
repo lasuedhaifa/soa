27b28b7d941ef4a4322c460cc6a5a74abb2fb76b
lasuedhaifa <147328017+lasuedhaifa@users.noreply.github.com>
2023-12-05 23:06:57 +0100

    Delete TPSOA_61 directory

--- TPSOA_61/src/main/java/repositories/JPAreposotry.java
+++
@@ -1,8 +0,0 @@
-package repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface CompteRepository extends JpaRepository<Compte, Long> {
-    List<Compte> findByType(TypeCompte type);
-}
